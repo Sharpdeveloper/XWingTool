377b3c7fb09341063588e30c23663f7eac5fdc0f
shaprdeveloper <s>
2015-12-25 20:13:43 +0100

    first

---
+++ XWingTool/XWingTool.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XWingTool.Core
+{
+    public class Class1
+    {
+    }
+}
